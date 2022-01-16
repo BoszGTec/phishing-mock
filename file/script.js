5ee5d379033a77b0851d701e234c1afb1c66b03a
BoszGTec <95701554+BoszGTec@users.noreply.github.com>
2022-01-16 17:33:41 +0700

    Create script.js

---
+++ file/script.js
@@ -0,0 +1,33 @@
+if (window.screen.width<1000){
+    var set="width: 60rem;"
+    var h1=document.getElementsByClassName("ct")[0]
+    h1.style=set ; h1=""
+
+    var set="font-size:4rem;"
+    var h1=document.getElementsByTagName("h1")[0]
+    h1.style=set ; h1=""
+
+    set="font-size:3rem;"
+    var lb=document.getElementsByClassName("form-label")[0]
+    lb.style=set
+    var lb=document.getElementsByClassName("form-label")[1]
+    lb.style=set
+    lb=""
+
+    set="font-size:2rem;"
+    var inp=document.getElementsByClassName("form-control")[0]
+    inp.style=set
+    var inp=document.getElementsByClassName("form-control")[1]
+    inp.style=set 
+    inp=""
+
+    set="font-size:1.5rem;"
+    var inp=document.getElementsByClassName("btn btn-primary")[0]
+    inp.style=set
+    inp=""
+
+    set="font-size:1.4rem;"
+    var ft=document.getElementsByClassName("form-text")[0]
+    ft.style=set 
+    ft=""
+}
